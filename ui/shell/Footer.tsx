6e2311b62cd3421fc4c8dc8b5405afb6994f55e7
Tristan Brewster <tristan@ir0k.com>
2026-04-20 11:38:16 -0600

    Update Footer.tsx

--- ui/shell/Footer.tsx
+++ ui/shell/Footer.tsx
@@ -12,53 +12,81 @@ import LandscapeImage from "@/public/images/landscape.webp";
 
 export default function Footer() {
   return (
-    <footer className="bg-background w-full p-6">
-      <div className="bg-spray w-full overflow-hidden rounded-3xl pt-16">
-        <div className="flex w-full flex-col items-start justify-between gap-16 px-8 sm:px-12 md:flex-row">
-          <div className="flex w-full flex-col gap-10">
-            <h2 className="text-heading-2 max-w-xl leading-[0.9] font-semibold">
-              Search, <GradientText>better.</GradientText>
-            </h2>
-            <div className="flex flex-row items-center gap-3">
-              <GlassButton
-                size="lg"
-                startIcon={<Apple className="invert dark:invert-0" />}
-                label="Download"
-              />
-              <GlassButton
-                as="a"
-                href="https://github.com/xplato/Sharkfin"
-                target="_blank"
-                size="lg"
-                startIcon={<Github className="invert dark:invert-0" />}
-                label="GitHub"
-              />
+    <footer className="bg-background relative w-full">
+      <div className="p-6">
+        <div className="bg-spray w-full overflow-hidden rounded-3xl pt-16">
+          <div className="flex w-full flex-col items-start justify-between gap-16 px-8 sm:px-12 md:flex-row">
+            <div className="flex w-full flex-col gap-10">
+              <h2 className="text-heading-2 max-w-xl leading-[0.9] font-semibold">
+                Search, <GradientText>better.</GradientText>
+              </h2>
+              <div className="flex flex-row items-center gap-3">
+                <GlassButton
+                  size="lg"
+                  startIcon={<Apple className="invert dark:invert-0" />}
+                  label="Download"
+                />
+                <GlassButton
+                  as="a"
+                  href="https://github.com/xplato/Sharkfin"
+                  target="_blank"
+                  size="lg"
+                  startIcon={<Github className="invert dark:invert-0" />}
+                  label="GitHub"
+                />
+              </div>
+            </div>
+
+            <div className="flex flex-col items-start justify-start gap-12 text-right md:items-end md:gap-16">
+              <div className="flex flex-col items-start justify-start gap-3 md:items-end">
+                <TextLink href="/privacy" size="md">
+                  Privacy
+                </TextLink>
+                <TextLink
+                  href="https://github.com/xplato/Sharkfin/issues"
+                  size="md"
+                >
+                  Report a bug
+                </TextLink>
+                <TextLink href="https://github.com/xplato/Sharkfin" size="md">
+                  Sharkfin source
+                </TextLink>
+                <TextLink
+                  href="https://github.com/xplato/Sharkfin.sh"
+                  size="md"
+                >
+                  Site source
+                </TextLink>
+              </div>
+
+              <div className="flex flex-col items-start gap-2 md:items-end">
+                <p className="flex flex-row items-center justify-center gap-2 text-base font-medium whitespace-nowrap">
+                  <span>Made with</span>
+                  <HeartIcon weight="fill" className="size-5 text-red-500" />
+                  <span>in Salt Lake City</span>
+                </p>
+                <p className="text-base font-medium">
+                  <span>Built by</span>{" "}
+                  <TextLink href="https://tristan.fyi">Tristan</TextLink>
+                </p>
+              </div>
             </div>
           </div>
 
-          <div className="flex flex-col items-start justify-start gap-8 text-right md:items-end md:gap-16">
-            <div className="flex flex-col items-start justify-start gap-3 md:items-end">
-              <TextLink href="/privacy" size="md">
-                Privacy
-              </TextLink>
-              <TextLink href="https://github.com/xplato/Sharkfin.sh" size="md">
-                View this site on GitHub
-              </TextLink>
-            </div>
-
-            <p className="flex flex-row items-center justify-center gap-2 text-base font-medium whitespace-nowrap">
-              <span>Made with</span>
-              <HeartIcon weight="fill" className="size-5 text-red-500" />
-              <span>in Salt Lake City</span>
-            </p>
+          <div className="mt-16 flex max-h-156 w-full items-start justify-start">
+            <Image
+              src={LandscapeImage}
+              alt="Landscape"
+              className="pointer-events-none opacity-0"
+            />
           </div>
         </div>
+      </div>
 
-        <div className="mt-16 flex max-h-128 w-full items-start justify-start">
-          <TooltipImage tooltipLabel="landscape.webp">
-            <Image src={LandscapeImage} alt="Landscape" />
-          </TooltipImage>
-        </div>
+      <div className="absolute right-0 bottom-0 left-0 max-h-156 w-full overflow-hidden">
+        <TooltipImage tooltipLabel="landscape.webp">
+          <Image src={LandscapeImage} alt="Landscape" />
+        </TooltipImage>
       </div>
     </footer>
   );
