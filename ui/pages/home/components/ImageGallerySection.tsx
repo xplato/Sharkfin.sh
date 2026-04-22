914aaf4835956c66aaacbd857378405969d07705
Tristan Brewster <tristan@ir0k.com>
2026-04-21 18:52:23 -0600

    Finalize image gallery

--- ui/pages/home/components/ImageGallerySection.tsx
+++ ui/pages/home/components/ImageGallerySection.tsx
@@ -6,13 +6,14 @@ import WithDecorativeGlassTooltip from "@/ui/WithDecorativeGlassTooltip";
 import { ImageType } from "@/lib/types";
 import { cn } from "@/lib/utils";
 
-import RelevanceImage3 from "@/public/images/app/tiger-in-pool.webp";
-import RelevanceImage4 from "@/public/images/app/vintage-chevy.webp";
-import RelevanceImage2 from "@/public/images/app/woman-as-flamingo.webp";
+import CherryBlossomImage from "@/public/images/app/cherry-blossom.webp";
+import PngFilterImage from "@/public/images/app/png-filter.webp";
+import SettingsImage from "@/public/images/app/settings.webp";
+import VintageChevyImage from "@/public/images/app/vintage-chevy.webp";
 
 export default function ImageGallerySection() {
   return (
-    <section className="bg-background flex w-full flex-col items-start gap-16 py-24 sm:py-32 md:py-40">
+    <section className="bg-background flex w-full flex-col items-start gap-12 py-28 sm:gap-16 sm:py-32 md:py-40">
       <div className="page-container items-center">
         <div className="w-full max-w-6xl">
           <h3 className="text-heading-4 leading-none font-semibold">
@@ -27,33 +28,43 @@ export default function ImageGallerySection() {
       <Slider ariaLabel="Sharkfin app gallery" className="flex flex-col gap-10">
         <GalleryItem
           image={{
-            src: RelevanceImage4,
+            src: VintageChevyImage,
             alt: "Sharkfin searchbar showing results for the search term 'vintage chevy'",
             tooltipLabel: "vintage-chevy.webp",
           }}
-          primaryText="Crazy fast."
-          secondaryText="Seriously"
+          primaryText="Relevant results in milliseconds."
+          secondaryText="All on your device—no cloud."
           className="max-w-sm sm:max-w-lg md:max-w-xl lg:max-w-3xl"
         />
         <GalleryItem
           image={{
-            src: RelevanceImage2,
-            alt: "Sharkfin searchbar showing results for the search term 'woman as flamingo'",
-            tooltipLabel: "woman-as-flamingo.webp",
+            src: PngFilterImage,
+            alt: "Sharkfin searchbar with the 'type' filter opened, selected to filter only PNG images",
+            tooltipLabel: "png-filter.webp",
           }}
-          primaryText="Crazy fast."
-          secondaryText="Seriously"
-          className="max-w-sm sm:max-w-md md:max-w-xl"
+          primaryText="Easy result filters, one click away."
+          secondaryText="Filter by search scope or file type—right in the searchbar."
+          className="max-w-sm sm:max-w-lg md:max-w-xl lg:max-w-3xl"
         />
         <GalleryItem
           image={{
-            src: RelevanceImage3,
-            alt: "Sharkfin searchbar showing results for the search term 'tiger in pool'",
-            tooltipLabel: "tiger-in-pool.webp",
+            src: CherryBlossomImage,
+            alt: "Sharkfin searchbar showing the detail view for the search term 'zucchini'",
+            tooltipLabel: "cherry-blossom.webp",
           }}
-          primaryText="Crazy fast."
-          secondaryText="Seriously"
-          className="max-w-sm sm:max-w-md md:max-w-xl"
+          primaryText="View file metadata."
+          secondaryText="Everything you need to know, right where you need it."
+          className="max-w-sm sm:max-w-lg md:max-w-xl lg:max-w-3xl"
+        />
+        <GalleryItem
+          image={{
+            src: SettingsImage,
+            alt: "Sharkfin settings interface",
+            tooltipLabel: "settings.webp",
+          }}
+          primaryText="Straightforward options."
+          secondaryText="Customize app functionality and manage added directories without hassle."
+          className="max-w-sm sm:max-w-lg md:max-w-xl lg:max-w-3xl"
         />
       </Slider>
     </section>
@@ -65,12 +76,19 @@ function GalleryItem({
   primaryText,
   secondaryText,
   className,
-}: {
-  image: ImageType;
-  primaryText: string;
-  secondaryText: string;
-  className: string;
-}) {
+}:
+  | {
+      image: ImageType;
+      primaryText: string;
+      secondaryText: string;
+      className: string;
+    }
+  | {
+      image: ImageType;
+      primaryText?: string;
+      secondaryText?: string;
+      className: string;
+    }) {
   return (
     <div
       className={cn("flex flex-col items-start justify-start gap-8", className)}
@@ -81,12 +99,14 @@ function GalleryItem({
         </WithDecorativeGlassTooltip>
       </div>
 
-      <div className="w-full px-6">
-        <p className="text-xl font-medium">
-          <span>{primaryText}</span>{" "}
-          <span className="text-foreground/45">{secondaryText}</span>
-        </p>
-      </div>
+      {primaryText && secondaryText && (
+        <div className="w-full max-w-xl px-6">
+          <p className="text-body font-medium">
+            <span>{primaryText}</span>{" "}
+            <span className="text-foreground/45">{secondaryText}</span>
+          </p>
+        </div>
+      )}
     </div>
   );
 }
